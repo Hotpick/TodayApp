4f7c293f06ae0805d18b2fc31299f8c32b4887a9
Yevhenii Romanyuk <hotpick18@gmail.com>
2022-12-05 16:47:25 +0200

    Initial Commit

---
+++ TodayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayApp
+//
+//  Created by Zhenya Romaniyk on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
